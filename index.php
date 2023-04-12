14d223abbd272b6f63791ba611dfdaef881bba0a
Akhil17-1 <123491479+Akhil17-1@users.noreply.github.com>
2023-04-12 18:04:09 -0400

    updated

--- index.php
+++ index.php
@@ -1,65 +1,34 @@
 <?php
-// start the session
 session_start();
-
+$sessionId = session_id();
 // insert config.php
-include('config.php');
-include('header.php');
-?>
 
+ include('config.php');
+ include('header.php');
+
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <title>My Eco-Friendly Website</title>
-        <style>
-   h1 {
-      text-align: center;
-    }
-                body {
-                        background-color: #c8e6c9; /* light green hex code */
-                        #background-image: url("images/eco-friendly.png");
-                        #background-size: cover;
-                        color: black;
-                }
-        p {
-                        font-size: 18px;
-                }
-        </style>
+  <title>My Ecommerce Website</title>
 </head>
 <body>
-  <header>
 
-  </header>
-  <main>
-    <section>
-<div style="text-align:center">
-<h1>Welcome to Greenkart</h1>
-</div>
-      <h2>Our Mission</h2>
-      <p>At My Eco-Friendly Website, we are committed to promoting sustainability and environmental awareness. Our mission is to provide high-quality eco-friendly products and resources to individuals and organizations who share our passion for protecting the planet.</p>
-      <p>From reusable bags to solar-powered gadgets, we offer a wide range of products that help reduce waste, conserve energy, and support a healthier planet. We also provide educational resources, tips, and inspiration to help our customers live more sustainably in their daily lives.</p>
-    </section>
-    <section>
-      <h2>Featured Content</h2>
-      <ul>
-        <li><a href="http://www.greeneducationfoundation.org/nationalgreenweeksub/waste-reduction-tips/tips-to-use-less-plastic.html">New blog post: 5 Easy Ways to Reduce Plastic Use</a></li>
-        <li><a href="https://www.emerald.com/insight/content/doi/10.1108/SAMPJ-07-2021-0260/full/html">In the news: Our CEO speaks about the importance of sustainability</a></li>
-        <li><a href="http://large.stanford.edu/courses/2015/ph240/wilczynski2/">Product spotlight: Solar-powered phone charger</a></li>
-      </ul>
-    </section>
-    <section>
-      <h2>Join Our Community</h2>
-      <p>Sign up for our newsletter to stay up-to-date on the latest eco-friendly news and products, and to receive exclusive discounts and promotions.</p>
-      <form action="#" method="post">
-        <label for="email">Enter your email:</label>
-        <input type="email" id="email" name="email" required>
-        <button type="submit">Subscribe</button>
-      </form>
-    </section>
-  </main>
-  <footer>
-    <p>&copy; 2023 My Eco-Friendly Website. All rights reserved.</p>
-  </footer>
+  <h1>Welcome to My Ecommerce Website</h1>
+  <?php if (isset($_SESSION['username'])) : ?>
+    <p>Welcome, <?php echo $_SESSION['username']; ?>!</p>
+    <p><a href="shop.php?session_id=<?php echo $sessionId; ?>">Shop Now</a></p>
+    <p><a href="logout.php">Logout</a></p>
+  <?php else : ?>
+    <p><a href="login.php">Login</a> or <a href="register.php">Register</a> to start shopping.</p>
+  <?php endif; ?>
+    <p?><a href="contact.php">contact</a></p>
+
+  <h2>Powered by Akhil Reddy Duvvuru:' </h2>
+
 </body>
 </html>
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~               
