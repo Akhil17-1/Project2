4dcb232d735a9c5761b6a5ea8a2356f474681304
Akhil17-1 <123491479+Akhil17-1@users.noreply.github.com>
2023-04-12 17:57:49 -0400

    updated

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 // DB credentials.
 define('DB_HOST','localhost');
 define('DB_USER','akhil');
-define('DB_PASS','akhil123');
+define('DB_PASS','akhil');
 define('DB_NAME','project2');
 // Establish database connection.
 $db = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
@@ -14,5 +14,6 @@ if (mysqli_connect_errno())
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 
-  
+
 ?>
+~   
