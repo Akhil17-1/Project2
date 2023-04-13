00aba9d6ad0fa3a60b7105a3e06f2a5412e725da
Akhil17-1 <123491479+Akhil17-1@users.noreply.github.com>
2023-04-12 23:45:22 -0400

    Update shop.php

--- shop.php
+++ shop.php
@@ -3,7 +3,7 @@ session_start();
 // insert config.php
 
 include('config.php');
- include('header.php');
+include('header.php');
 
 $query = "SELECT * FROM products";
 $results = mysqli_query($db, $query);
@@ -13,33 +13,53 @@ $results = mysqli_query($db, $query);
 <html>
 <head>
   <title>Shop - My Ecommerce Website</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-  <h1>Shop</h1>
-  <?php if (isset($_SESSION['username'])) : ?>
-    <p>Welcome, <?php echo $_SESSION['username']; ?>!</p>
-    <p><a href="logout.php">Logout</a></p>
-  <?php else : ?>
-    <p><a href="login.php">Login</a> or <a href="register.php">Register</a> to start shopping.</p>
-  <?php endif; ?>
-  <h2>Products</h2>
-  <table>
-    <thead>
-      <tr>
-        <th>Product Name</th>
-        <th>Price</th>
-        <th>Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php while ($row = mysqli_fetch_assoc($results)) : ?>
-        <tr>
-          <td><?php echo $row['name']; ?></td>
-          <td><?php echo $row['price']; ?></td>
-          <td><a href="add_to_cart.php?id=<?php echo $row['id']; ?>">Add to Cart</a></td>
-        </tr>
-      <?php endwhile; ?>
-    </tbody>
-  </table>
+  <div class="container">
+    <h1>Shop</h1>
+    <?php if (isset($_SESSION['username'])) : ?>
+      <p class="welcome-msg">Welcome, <?php echo $_SESSION['username']; ?>!</p>
+      <p><a href="logout.php" class="logout-link">Logout</a></p>
+    <?php else : ?>
+      <p><a href="login.php">Login</a> or <a href="register.php">Register</a> to start shopping.</p>
+    <?php endif; ?>
+    <div class="product-list">
+      <h2>Products</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Product Name</th>
+            <th>Price</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php while ($row = mysqli_fetch_assoc($results)) : ?>
+            <tr>
+              <td><?php echo $row['name']; ?></td>
+              <td>$<?php echo $row['price']; ?></td>
+              <td><a href="add_to_cart.php?id=<?php echo $row['id']; ?>">Add to Cart</a></td>
+            </tr>
+          <?php endwhile; ?>
+          <tr>
+            <td>Apple iPhone 13</td>
+            <td>$999.99</td>
+            <td><a href="add_to_cart.php?id=5">Add to Cart</a></td>
+          </tr>
+          <tr>
+            <td>Samsung Galaxy Watch 4</td>
+            <td>$349.99</td>
+            <td><a href="add_to_cart.php?id=6">Add to Cart</a></td>
+          </tr>
+          <tr>
+            <td>Bose QuietComfort Earbuds</td>
+            <td>$279.99</td>
+            <td><a href="add_to_cart.php?id=7">Add to Cart</a></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </body>
 </html>
